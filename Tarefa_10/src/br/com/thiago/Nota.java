89bbb7a322b7e71df143daaa6a807cc52f2301d1
Thiago Araujo <thiago.araujo@sinqia.com.br>
2022-08-24 18:59:23 -0300

    Tarefa_10_01

--- Tarefa_10/src/br/com/thiago/Nota.java
+++ Tarefa_10/src/br/com/thiago/Nota.java
@@ -11,17 +11,22 @@ public class Nota {
 
         System.out.println("Qual é seu nome? ");
         String nome1 = s.next();
-        System.out.println("Prazer " +nome1);
-        System.out.println("Vamos Começar? digite 1 para começar ");
-        int nome = s.nextInt();
+        System.out.println("Prazer " +nome1 + " Vamos Começar? ");
 
-        String nomeBim = getBimestre(nome);
-        String nome2 = getNota(nome);
-        System.out.println(nome2);
+        String resposta = s.next();
 
+        while (resposta.equalsIgnoreCase("Sim")) {
+
+            getBimestre();
+            System.out.println("Dgite a média Final");
+            double nome = s.nextDouble();
+            String nome2 = getNota(nome);
+            System.out.println(nome2);
+        }
+        System.out.println("Obrigado. ");
 
     }
-    public  static String getBimestre(int nome){
+    public  static void getBimestre(){
         Scanner s = new Scanner(System.in);
 
         System.out.println("Entre com a nota Primeiro Bimestre ");
@@ -33,19 +38,22 @@ public class Nota {
         System.out.println("Entre com a nota Quarto Bimestre ");
         double quartoBimestre = s.nextDouble();
 
-        double getBimestre = (primeiroBimestre + segundoBimestre + terceiroBimestre + quartoBimestre )/4;
-        System.out.println("Media Final " + getBimestre);
-        return "";
+        double mediaBimestre = (primeiroBimestre + segundoBimestre + terceiroBimestre + quartoBimestre )/4;
+        System.out.println("Media Final " + mediaBimestre);
+
+
 
     }
-    public static  String getNota(int nome){
-        if(nome >=7 && nome <=10){
+    public static String getNota(double nome) {
+
+        if (nome >= 7 && nome <= 10) {
             return "Aprovado";
         } else if (nome >= 5 && nome <= 6) {
             return "Recuperacao";
-        }else{
+        } else {
             return "Reprovado";
         }
-
     }
+
+
 }
